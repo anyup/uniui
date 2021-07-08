f53611ed9106bd2dc6e52903a79ce8b284608198
qiaomingxing <491302297@qq.com>
2021-07-08 15:51:30 +0800

    feat: pager

--- src/core/class/pager.js
+++ src/core/class/pager.js
@@ -6,51 +6,86 @@ class Pager {
     this.total = 0 // 总数量
     this.data = [] // 数据
   }
+
+  get offset() {
+    return this.data.length
+  }
+
+  get empty() {
+    return this.data.length === 0
+  }
+
   setPage(page) {
     this.page = page
     return this
   }
+
   setLimit(limit) {
     this.limit = limit
     return this
   }
-  setData(data) {
-    this.data = data
+
+  setData(data, reset = false) {
+    if (reset) {
+      this.data = data
+    } else {
+      this.data = [...this.data, ...data]
+    }
     return this
   }
+
   setPages(pages) {
     this.pages = pages
     return this
   }
+
   setTotal(total) {
     this.total = total
     return this
   }
+
   pagePlus() {
     this.page++
     return this
   }
+
   pageMinus() {
     this.page--
     return this
   }
+
   isEmpty() {
     return this.data.length === 0
   }
-  hasMore() {
+
+  hasMore(offset = false) {
+    if (offset) {
+      return this.offset < this.total
+    }
     return this.page < this.pages
   }
+
   // 加载状态: more,loading,noMore
-  getLoadStatus() {
-    if (this.data.length === 0 || this.pages <= 1) {
-      return ''
+  getLoadStatus(offset = false) {
+    let status = ''
+    if (offset) {
+      if (this.data.length === 0 || this.offset === 0) {
+        status = ''
+      } else if (this.offset >= this.total) {
+        status = 'nomore'
+      } else {
+        status = 'loading'
+      }
+    } else {
+      if (this.data.length === 0 || this.pages <= 1) {
+        status = ''
+      } else if (this.page >= this.pages) {
+        status = 'nomore'
+      } else {
+        status = 'loading'
+      }
     }
-    if (this.page >= this.pages) {
-      return 'nomore'
-    }
-    if (this.data.length > 0) {
-      return 'loading'
-    }
+    return status
   }
 }
 
