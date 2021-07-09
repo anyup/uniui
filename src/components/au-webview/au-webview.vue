31c4cce3b1165c1474dc0509f8f222b676803429
qiaomingxing <491302297@qq.com>
2021-07-09 10:41:09 +0800

    feat: webview

---
+++ src/components/au-webview/au-webview.vue
@@ -0,0 +1,33 @@
+<template>
+  <web-view :webview-styles="styles" :src="src"></web-view>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    progressColor: {
+      type: String,
+      default: '#2979ff'
+    },
+    webviewStyles: {
+      type: Object,
+      default: {}
+    }
+  },
+  computed: {
+    styles() {
+      let { progress } = this.webviewStyles
+      if (progress && !progress.color) {
+        progress.color = this.progressColor
+      }
+      return [...webviewStyles, ...{ progress }]
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
