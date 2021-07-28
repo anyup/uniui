bba53cbaae964440e4b4acb5d9b0dcbc07eb8c28
qiaomingxing <491302297@qq.com>
2021-07-28 11:21:39 +0800

    0.0.19 优化防抖节流类

--- src/core/class/Optimize.js
+++ src/core/class/Optimize.js
@@ -1,77 +1,33 @@
 class Builder {
-  constructor(callback, delay) {
-    this.callback = callback
+  constructor(delay = 500) {
     this.delay = delay
+    this.previous = 0
+    this.timeout = undefined
   }
 
-  setCallback(callback) {
-    this.callback = callback
-    return this
-  }
-
-  getCallback() {
-    return this.callback
-  }
-
-  setDelay(delay) {
-    this.delay = delay
-    return this
-  }
-
-  getDelay() {
-    return this.delay
-  }
-
-  build(type) {
-    if (type === 'debounce') {
-      return new DebounceOptimize(this)
-    } else if (type === 'throttle') {
-      return new ThrottleOptimize(this)
+  // 节流
+  throttle(callback, delay) {
+    delay = delay || this.delay
+    let now = Date.now()
+    if (now - this.previous > delay) {
+      callback()
+      this.previous = now
     }
+  }
+
+  // 防抖
+  debounce(callback, delay) {
+    delay = delay || this.delay
+    if (this.timeout) clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      callback()
+    }, delay)
   }
 }
 //优化基类
 class Optimize {
   static Builder = Builder
-  constructor() {
-    this.callback = null
-    this.delay = 500
-  }
-  excute(params) {
-    // do something......
-  }
-}
-//防抖
-class DebounceOptimize extends Optimize {
-  constructor(builder) {
-    super()
-    this.callback = builder.callback
-    this.delay = builder.delay
-    this.timeout = undefined
-  }
-  excute(params) {
-    if (this.timeout) clearTimeout(this.timeout)
-    this.timeout = setTimeout(() => {
-      this.callback(params)
-    }, this.delay)
-  }
-}
-
-//节流
-class ThrottleOptimize extends Optimize {
-  constructor(builder) {
-    super()
-    this.callback = builder.callback
-    this.delay = builder.delay
-    this.previous = 0
-  }
-  excute(params) {
-    let now = Date.now()
-    if (now - this.previous > this.delay) {
-      this.callback(params)
-      this.previous = now
-    }
-  }
+  constructor() {}
 }
 
 export { Optimize }
