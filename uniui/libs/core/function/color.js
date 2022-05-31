c2e629fc00155c24cd66a160c5df672934d4bc45
qiaomingxing <491302297@qq.com>
2022-05-31 16:21:30 +0800

    feat: 修改js不支持导入css的问题

--- uniui/libs/core/function/color.js
+++ uniui/libs/core/function/color.js
@@ -1,9 +1,14 @@
-import style from '../../../theme.scss'
+// import style from '../../../theme.scss'
 
 export default {
-  primary: style.primary,
-  warning: style.warning,
-  success: style.success,
-  error: style.error,
-  info: style.info
+  // primary: style.primary,
+  // warning: style.warning,
+  // success: style.success,
+  // error: style.error,
+  // info: style.info
+  primary: '#2979ff',
+  warning: '#ff9900',
+  success: '#19be6b',
+  error: '#fa3534',
+  info: '#909399'
 }
