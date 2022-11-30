f72bc6468c026c3a86260a959756583b611942a7
qiaomingxing <491302297@qq.com>
2022-11-30 17:36:44 +0800

    feat: api demo

--- pages/components/apis.vue
+++ pages/components/apis.vue
@@ -1,10 +1,10 @@
 <template>
   <app-layout>
     <config-demo>
-      <view class="is-pdb-10 is-border">
+      <view class="is-pdb-10 is-border is-text-left">
         <au-apis :list="apiList" @select="setServer" />
       </view>
-      <view v-if="value" class="is-pd-10 is-mgt-20 is-border">
+      <view v-if="value" class="is-pd-10 is-mgt-20 is-border is-text-left">
         <view>名称：{{ value.name }}</view>
         <view>地址：{{ value.domain }}</view>
         <view>端口：{{ value.port }}</view>
@@ -18,21 +18,21 @@ const apiList = [
   {
     name: '生产环境',
     children: [
-      { id: 'domain', value: 'https://api.prod.com' },
+      { id: 'domain', value: 'https://api.production.com' },
       { id: 'port', value: '8080' }
     ]
   },
   {
-    name: 'Test',
+    name: '开发环境',
+    children: [
+      { id: 'domain', value: 'https://api.development.com' },
+      { id: 'port', value: '8080' }
+    ]
+  },
+  {
+    name: '测试环境',
     children: [
       { id: 'domain', value: 'https://api.test.com' },
-      { id: 'port', value: '8080' }
-    ]
-  },
-  {
-    name: 'Test1',
-    children: [
-      { id: 'domain', value: 'https://api.test1.com' },
       { id: 'port', value: '8080' }
     ]
   }
