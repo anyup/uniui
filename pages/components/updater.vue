f993d58f97832674c266d2889a082f5179a5c81c
qiaomingxing <491302297@qq.com>
2024-11-05 17:06:48 +0800

    feat: 版本更新

--- pages/components/updater.vue
+++ pages/components/updater.vue
@@ -1,6 +1,6 @@
 <template>
   <app-layout>
-    <config-demo v-model="value" :list="list" tips="为保证演示的正确性，请在真机环境下测试，H5仅为演示">
+    <config-demo v-model="value" :list="list" tips="为保证演示的正确性，请在真机环境下测试，本模块仅为演示更新效果">
       <!-- #ifdef APP-PLUS -->
       <view class="is-flex is-flex-column is-pd-10 is-mgtb-10 is-border is-radius-8 is-text-left">
         <view>App版本</view>
@@ -11,17 +11,10 @@
         <text>{{ wgtVersion }}</text>
       </view>
       <!-- #endif -->
-      <app-button type="primary" @click="checkUpdate">检测版本更新（仅测试）</app-button>
+      <app-button type="primary" @click="checkUpdate">检测版本更新</app-button>
+      <view class="is-text-center is-font-12">请求接口仅作为测试展示效果</view>
     </config-demo>
-    <au-updater
-      ref="updater"
-      :auto="false"
-      :request="request"
-      :modalTitle="value.modalTitle"
-      :modalIcon="value.modalIcon"
-      @success="onResult"
-      @modal-confirm="onConfirm"
-    />
+    <au-updater ref="updater" :auto="false" :request="request" :modalTitle="value.modalTitle" :modalIcon="value.modalIcon" @success="onResult" @modal-confirm="onConfirm" />
   </app-layout>
 </template>
 
@@ -31,18 +24,12 @@ import { Http } from '@/uniui'
 export default {
   data() {
     return {
-      request: {
-        header: { 'Content-Type': 'application/json' },
-        url: '/api/appVersionInfo/getAppUpgradeInfo',
-        params: { applicationCode: 'BiolinkApp', versionNum: 1, platformType: 0 },
-        method: 'POST'
-      },
       list: [
         {
           type: 'modalTitle',
           title: '弹出框标题: modalTitle',
           layout: 'radio',
-          list: ['发现新版本', '版本更新']
+          list: ['发现新版本', '版本更新提示']
         },
         {
           type: 'modalIcon',
@@ -60,13 +47,14 @@ export default {
         {
           type: 'hot',
           title: '是否热更新: hot',
+          tips: '检测到有新版本，自动下载更新资源并主动更新应用',
           layout: 'radio',
           list: [false, true]
         },
         {
           type: 'slient',
           title: '静默更新: slient（仅在热更新模式下有效）',
-          tips: '是否静默更新（静默更新无需用户操作）',
+          tips: '静默更新无需用户操作，检测到有新版本资源后，自动下载安装',
           layout: 'radio',
           list: [false, true]
         }
@@ -80,7 +68,9 @@ export default {
       },
       appVersion: '',
       wgtVersion: '',
-      request: undefined
+      http: new Http(),
+      // request 为promise
+      request: null
     }
   },
   onLoad(options) {
@@ -88,11 +78,16 @@ export default {
     this.setRequest()
   },
   methods: {
+    // 设置请求 promise
     setRequest() {
-      const url = '/api/appVersionInfo/getAppUpgradeInfo'
+      let url = '/static/json/app-version.json'
+      // #ifdef H5
+      url = '/h5/static/json/app-version.json'
+      // #endif
       const header = { 'Content-Type': 'application/json' }
-      const params = { applicationCode: 'BiolinkApp', versionNum: 1, platformType: 2 }
-      this.request = () => new Http().setHeader(header).post(url, params)
+      // versionCode 为获取的当前版本号
+      const params = { versionCode: 1 }
+      this.request = () => new Http().setHeader(header).get(url, params)
     },
     // 获取应用信息
     getAppInfo() {
@@ -106,54 +101,87 @@ export default {
       })
       // #endif
     },
+    // 检查更新
     checkUpdate() {
-      // #ifdef H5
-      this.showModal({
-        versionDownloadUrl: 'https://www.anyup.cn/docs/',
-        versionDesc: '1.新增功能模块\n2.优化已知问题优化\n3.提升应用性能',
-        versionName: '1.1.9'
-      })
-      // #endif
-      // #ifdef APP-PLUS
+      // #ifndef MP-WEIXIN
+      this.$tips.loading()
       this.$refs.updater.checkUpdate()
       // #endif
+      // #ifdef MP-WEIXIN
+      this.showModal({
+        versionName: '3.6.0',
+        versionContent: '1.新增功能模块\n2.优化已知问题优化\n3.提升应用性能',
+        versionDownloadUrl: 'https://mp.weixin.qq.com/s/bgkhC02WPlEHmGnCbSmDcg'
+      })
+      // #endif
+    },
+    // 请求接口
+    doRequest() {
+      // #ifdef MP-WEIXIN
+      this.onResult({
+        data: {
+          versionCode: 6,
+          versionName: '3.6.0',
+          versionContent: '1.新增功能模块\n2.优化已知问题优化\n3.提升应用性能',
+          versionDownloadUrl: 'https://mp.weixin.qq.com/s/bgkhC02WPlEHmGnCbSmDcg'
+        }
+      })
+      return
+      // #endif
+      let url = '/static/json/app-version.json'
+      // #ifdef H5
+      url = '/h5/static/json/app-version.json'
+      // #endif
+      const header = { 'Content-Type': 'application/json' }
+      // versionCode 为获取的当前版本号
+      const params = { versionCode: 1 }
+      this.http
+        .setHeader(header)
+        .get(url, params)
+        .then(res => {
+          this.onResult(res)
+        })
+        .catch(err => {
+          this.$tips.loaded()
+          this.$tips.toast(JSON.stringify(err))
+        })
     },
     // 接口请求成功回调
     onResult(response) {
-      console.log(response)
-      /* #ifdef H5 */
-      this.$tips.toast('请在真机环境下测试')
-      /* #endif */
-      /* #ifdef APP-PLUS */
       const data = response.data
+      // #ifdef APP-PLUS
       if (this.value.hot && this.value.slient) {
         this.$refs.updater.hotUpdate(data.versionDownloadUrl)
         return
       }
+      // #endif
+      this.$tips.loaded()
       this.showModal(data)
-      /* #endif */
     },
     // 显示更新弹窗提示
-    showModal({ versionDownloadUrl, versionDesc, versionName }) {
+    showModal({ versionDownloadUrl, versionContent, versionName }) {
       this.$refs.updater.showModal({
         url: versionDownloadUrl,
         version: versionName,
-        content: versionDesc.replace(/<zh>(.*?)<\/zh>/g, '$1').replace(/\|/g, '\n'),
+        content: versionContent.replace(/<zh>(.*?)<\/zh>/g, '$1').replace(/\|/g, '\n'),
         force: this.value.force
       })
     },
     // 弹窗确认事件
     onConfirm(url) {
-      // #ifdef H5
-      this.$refs.updater.closeModal()
-      this.$tips.toast('请在真机环境下测试', 'info')
-      // #endif
       // #ifdef APP-PLUS
       if (this.value.hot) {
+        // 是否热更新
         this.$refs.updater.hotUpdate(url)
       } else {
         plus.runtime.openURL(url)
       }
+      // #endif
+      // #ifdef H5
+      window.open(url)
+      // #endif
+      // #ifdef MP-WEIXIN
+      this.$u.route(`/pages/components/webview?src=${url}`)
       // #endif
     }
   }
